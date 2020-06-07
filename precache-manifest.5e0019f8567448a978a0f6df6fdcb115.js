4461e939aad3729c51561f5c2b47baa8e1ac85ea
shewolf22 <ktiwari888@gmail.com>
2020-06-07 10:35:19 +0530

    Updates

---
+++ precache-manifest.5e0019f8567448a978a0f6df6fdcb115.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc2282c798ca285741738d5573b4f7c5",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1f5cb38a6e8e7d9f1980",
+    "url": "/monsters-rolodex/static/css/main.1e7724a5.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f5cb38a6e8e7d9f1980",
+    "url": "/monsters-rolodex/static/js/main.915a3303.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
